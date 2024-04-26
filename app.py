d18bab21227eab56903c3281a8c23dde8a90aaa5
JorgeEstebanGil <jestebangil2001@gmail.com>
2024-04-26 12:59:21 +0200

    app works (cool)

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #python -m streamlit run app.py
 import pandas as pd
-import pandas_datareader as data
+from pandas_datareader import data as pdr
 import numpy as np
 import matplotlib.pyplot as plt
 import yfinance as yf  
@@ -8,16 +8,34 @@ from keras.models import load_model
 import streamlit as st
 import datetime as dt
 
-start = dt.datetime(2012,1,1)
-end = dt.datetime(2024,1,1)
-#datetime.datetime.now().strftime('%Y-%m-%d')
+#Variables
+max_end = dt.datetime.now().date()
+min_start = dt.datetime(2012,1,1).date()
 
+#Título
 st.title('Stock Price Prediction App')
-user_input = st.text_input('Enter the stock ticker:', 'AAPL')
 
-#df = data.DataReader(user_input, 'yahoo', start, end)
-df = data.DataReader( user_input, data_source='yahoo', start=start, end=end)
+#Preparacion Datos
+yf.pdr_override()
+user_input = st.text_input('Enter the stock ticker:', 'AAPL')
+start_column, end_column = st.columns(2)
+
+with start_column:
+    start = st.date_input("Start date", min_value=min_start, max_value=max_end, value=min_start)
+
+with end_column:
+    end = st.date_input("End date", min_value=start, max_value=max_end, value=max_end)
+    
+#Fetching the data
+df = pdr.get_data_yahoo(user_input,start,end)
 
 #Describing data
-st.subheader('Data from 2012 - To today')
-st.write(df.describe())
+st.subheader(user_input + " Stock data from " + str(start))
+st.table(df.describe())
+
+#Visualizaciones
+st.subheader('Closing price vs Time chart')
+ma100 = df.Close.rolling(100).mean
+fig = plt.figure(figsize=(12,6))
+plt.plot(df.Close)
+st.pyplot(fig)
