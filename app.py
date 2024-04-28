053ed02b7bdaa8c985bf17171e3a6c77f0b68b65
JorgeEstebanGil <jestebangil2001@gmail.com>
2024-04-28 23:30:18 +0200

    Some details

--- app.py
+++ app.py
@@ -34,27 +34,38 @@ st.subheader(user_input + " Stock data from " + str(start))
 st.table(df.describe())
 
 #Volatility
-volatility = df['Close'].pct_change().std() * np.sqrt(252)  # Annualized volatility assuming 252 trading days per year
+volatility = df['Close'].pct_change().std() * np.sqrt(252) # Annualized volatility assuming 252 trading days per year
 color = 'off'
 if volatility > 0.75:  # Example threshold for high volatility
     color = 'inverse'
 elif volatility < 0.25:  # Example threshold for low volatility
     color = 'normal'
-    
+
+#Market Capitalization
 last_close_price = df['Close'][-1]
 shares_outstanding = 10_000_000  # Example number of shares outstanding
-market_cap = last_close_price * shares_outstanding
-
-# Display market capitalization in a Streamlit card
-st.subheader('Market Capitalization')
-st.metric("Market Cap", market_cap)
+market_cap = format(last_close_price * shares_outstanding, '.2f')
     
-st.metric("Volatility", " ", delta=volatility, delta_color=color)
+#Useful measures
+col1, col2, col3 = st.columns(3)
 
+with col1:
+    st.subheader('Volatility')
+    st.metric(" ", volatility)
+
+with col2:
+    st.subheader('Market Capitalization')
+    st.metric(" ", market_cap)
+
+with col3:
+    # Return avergae Daily Percentage Change
+    st.subheader('Average Daily Percentage Change')
+    st.metric(" ", df['Close'].pct_change().mean())
+    
 #Visualizaciones
 st.subheader('Closing price vs Time chart')
+fig = plt.figure(figsize=(10,6))
 ma100 = df.Close.rolling(100).mean()
-fig = plt.figure(figsize=(12,6))
 plt.plot(ma100)
 plt.plot(df.Close)
 plt.plot(ma100, label='100-Day Moving Average')
