db2df33ac0261fee7cacec3a6fe3ed786e40fdcb
JorgeEstebanGil <jestebangil2001@gmail.com>
2024-05-10 14:40:03 +0200

    News (almost) and Login creation

--- pages/News2.py
+++ pages/News2.py
@@ -12,10 +12,18 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import yfinance as yf  
 import datetime as dt
+import requests
+import io
+from PIL import Image 
 #Variables
 max_end = dt.datetime.now().date()
 min_start = dt.datetime(2012,1,1).date()
 
+def show_image_from_link(link, caption=None):
+    r = requests.get(link)
+    img = Image.open(io.BytesIO(r.content))
+    st.image(img, caption=caption, use_column_width=True)
+    
 #Título
 st.title('Stock Price Prediction App')
 
@@ -30,11 +38,13 @@ df = pdr.get_data_yahoo(Ticker)
 
 #Describing data
 st.subheader(stock_data.info['longName']+"("+Ticker)
-
-st.write(stock_data.news)
+st.write(stock_data.news[1])
 for i in range(len(stock_data.news)):
-    st.title(stock_data.news[i]['title'], anchor=stock_data.news[i]['link'])
-    st.subheader(stock_data.news[i]['publisher'])
+    st.header(stock_data.news[i]['title'], anchor=stock_data.news[i]['link'])
+    st.caption(stock_data.news[i]['publisher'] + " for " + stock_data.news[i]['publisher'])
+    st.write(stock_data.news[i]['link'])
+    if stock_data.news[i]['thumbnail']:
+        show_image_from_link(stock_data.news[i]['thumbnail']['resolutions'][0]['url'])
     for j in range(len(stock_data.news[i]['relatedTickers'])):
         st.caption(stock_data.news[i]['relatedTickers'][j])
 
